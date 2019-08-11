76413f772e54d38a476271e3de723e6ceb48b279
Nitin Bajpai <nitinbajpai110@gmail.com>
2019-08-11 14:58:43 +0530

    HEADER PART DONE

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import './App.css';
 
 function App() {
   return (
+      React.createElement("div",null,
+      React.createElement("header",{id:'header'},
+      React.createElement("button",{id:'head'},"Show Latest News")),
+      React.createElement("div",{id:'body'},
+      React.createElement("div",{id:'filter'},
+      React.createElement("div",{id:'head2'},
+      React.createElement("div",{id:'head1'},"Filter News"),
+      React.createElement("a",{id:'reset',href:""},"Reset"),
+      ),
+      React.createElement("label",{id:'label1'},"LANGUAGE")
+    
 
-      React.createElement("h1",null,"APPP")
-   
+      )
+      )
+      )
+     
   );
 }
 
